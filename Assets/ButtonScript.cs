6a86e5000c41c09cc140dbf354d15d5199829e35
furkaninalcik <m.furkaninalcik@gmail.com>
2018-07-19 17:01:12 +0300

    facebook authentication is working in unity editor but fails while building

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using Firebase;
+using Firebase.Auth;
+using Firebase.Unity.Editor;
+
+using Facebook.Unity;
+using Facebook;
+using FacebookPlatformServiceClient;
+
+
+public class ButtonScript : MonoBehaviour
+{
+    private void OnFacebookInitialized()
+    {
+        Debug.Log("Logging into Facebook...");
+
+        // Once Facebook SDK is initialized, if we are logged in, we log out to demonstrate the entire authentication cycle.
+        if (FB.IsLoggedIn)
+            FB.LogOut();
+
+        List<string> permissions = new List<string> { "public_profile", "user_friends", "email" };
+
+        // We invoke basic login procedure and pass in the callback to process the result
+        FB.LogInWithReadPermissions(permissions, AuthCallback);
+    }
+
+    private void Awake()
+    {
+        /*if (!FB.IsInitialized)
+        {
+            FB.Init();
+        }
+        else
+        {
+            FB.ActivateApp();
+        }*/
+
+        FB.Init(OnFacebookInitialized);
+
+
+        if (!FB.IsInitialized)
+        {
+            // Initialize the Facebook SDK
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else
+        {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
+    }
+
+
+    void Start()
+    {
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                Debug.Log("Connection with Firebase established");
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                Debug.Log("Connection with Firebase failed");
+            }
+        });
+    }
+
+
+    private void InitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else
+        {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else
+        {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+
+
+    public void facebookLogIn()
+    {
+        var perms = new List<string>() { "public_profile", "email" };
+        FB.LogInWithReadPermissions(perms, AuthCallback);
+    }
+
+
+    private void AuthCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            // AccessToken class will have session details
+            var accessToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            // Print current access token's User ID
+            Debug.Log(accessToken.UserId);
+            // Print current access token's granted permissions
+            foreach (string perm in accessToken.Permissions)
+            {
+                Debug.Log(perm);
+            }
+
+            FBFirebaseAuth(accessToken);
+
+        }
+        else
+        {
+            Debug.Log("User cancelled login");
+        }
+    }
+
+
+    public void FBFirebaseAuth(AccessToken accessToken)
+    {
+
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+        Debug.Log(FB.IsInitialized);
+
+        Debug.Log("Facebook is clicked");
+
+        Firebase.Auth.Credential credential = Firebase.Auth.FacebookAuthProvider.GetCredential(accessToken.TokenString);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+
+            Debug.Log(credential.ToString());
+            Debug.Log(task.ToString());
+
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+            Debug.Log(newUser.Email);
+        });
+
+    }
+}
